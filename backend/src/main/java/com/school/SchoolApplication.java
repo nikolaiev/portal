9fcfd0e57b35a6c54e3322dfd8efcd1989438bb4
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2018-01-30 00:32:57 +0200

    Initial commit

---
+++ backend/src/main/java/com/school/SchoolApplication.java
@@ -0,0 +1,17 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/com/school/config/StaticResourceConfiguration.java
@@ -0,0 +1,13 @@
+package com.school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("file:frontend/");
+    }
+}
