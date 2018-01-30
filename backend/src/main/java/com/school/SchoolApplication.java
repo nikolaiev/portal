12045239b7b248bcf85f4f1562d9982ffc6dc446
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2018-01-30 15:46:14 +0200

    Build configured

--- backend/src/main/java/com/school/SchoolApplication.java
+++ backend/src/main/java/com/school/SchoolApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,5 +14,18 @@ public class SchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+	// Enable CORS globally
+	// Enable Vue.js to make requests from localhost:8080
+	@Profile("!PROD")
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 }
