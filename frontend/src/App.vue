2f36647b226b371caabe2759badaff0a865e401d
Vladyslav Herasymchyk <slaughternal@gmail.com>
2018-01-31 03:14:49 +0200

    StartLogin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+
   </div>
 </template>
 
@@ -18,6 +19,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
+
 </style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <form action="" method="POST">
+      <p>Input your Login: <input type = "text" id = "Login"/></p>
+      <p>Input your Password: <input type = "text" id = "Password"/></p>
+      <button type = "submit">
+        <img src = "https://cs.pikabu.ru/post_img/2013/06/03/0/1370204436_2065417852.gif">
+      </button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Come in my classroom'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  p{
+    color : revert;
+    text-align: left;
+  }
+</style>
