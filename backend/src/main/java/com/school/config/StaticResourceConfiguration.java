524f502dd58d406f9942cee802fd127c5cc31717
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2018-02-03 02:45:54 +0200

    Defaul Swagger 2 configuration added

--- backend/src/main/java/com/school/config/StaticResourceConfiguration.java
+++ backend/src/main/java/com/school/config/StaticResourceConfiguration.java
@@ -9,5 +9,12 @@ public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/public/");
+
+        /*Actual only for !PROD spring.profile*/
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
