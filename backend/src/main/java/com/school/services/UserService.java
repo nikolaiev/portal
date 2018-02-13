5f7a8b337336afecc7e8a2dc10b11de6e54a177b
andrew <Andriy.Iwanyuk@gmail.com>
2018-02-13 02:07:46 +0200

    Add REST API functionality for teacher resource

--- backend/src/main/java/com/school/services/UserService.java
+++ backend/src/main/java/com/school/services/UserService.java
@@ -1,40 +1,8 @@
 package com.school.services;
 
 import com.school.entities.User;
-import com.school.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
 
-import java.util.Optional;
-
-@Service
-public class UserService implements GenericService<User> {
-    @Autowired
-    UserRepository userRepository;
-
-    @Override
-    public Optional<User> getById(long id) {
-        return Optional.ofNullable(userRepository.findOne(id));
-    }
-
-    @Override
-    public Iterable<User> getAllPageOffsetSize(int pageOffset, int pageSize) {
-        return userRepository.findAll(new PageRequest(pageOffset,pageSize));
-    }
-
-    @Override
-    public User create(User one) {
-        return userRepository.save(one);
-    }
-
-    @Override
-    public User update(User one) {
-        return userRepository.save(one);
-    }
-
-    @Override
-    public void deleteById(long id) {
-        userRepository.delete(id);
-    }
+public interface UserService extends GenericService<User> {
+    Page<User> getAllUsersByRole(String role, int pageOffset, int pageSize);
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO Event(id,title,text) VALUES (1,'Test event','Information about event');
-INSERT INTO USERS   ( EMAIL , MIDDLE_NAME , NAME ,PASSWORD ,SURNAME,BIRTHDAY ) VALUES ('test@i.ua','Volodymirovych','Volodymir','admin','Putin',123123);
+INSERT INTO USERS   ( EMAIL , MIDDLE_NAME , NAME ,PASSWORD ,SURNAME,BIRTHDAY, USER_TYPE) VALUES ('test@i.ua','Volodymirovych','Volodymir','admin','Putin',123123, 'ADMIN'),
+  ('test2@i.ua','Volodymirovych2','Volodymir2','admin','Putin2',123123, 'TEACHER'),
+  ('test3@i.ua','Volodymirovych3','Volodymir3','admin','Putin3',123123, 'TEACHER');
